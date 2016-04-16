cbed0f5f929a5c261f8fad83f365e93d59249ca0
Sebastian-0 <melon.bag@hotmail.com>
2016-04-16 15:19:18 +0200

    Split player control script in two

--- Ludum Dare 35/Assets/PlayerJumpControls.cs
+++ Ludum Dare 35/Assets/PlayerJumpControls.cs
@@ -1,21 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerControls : MonoBehaviour {
+public class PlayerJumpControls : MonoBehaviour {
 
-	[Range(0f, 25f)]
-	public float MOVEMENT_SPEED;
 	[Range(0f, 25f)]
 	public float JUMP_SPEED;
 
 	Rigidbody2D playerBody;
 	Animator animator;
 	bool pressedJump;
-    bool releasedJump;
+	bool releasedJump;
 
-    // Use this for initialization
-    void Start () {
-		MOVEMENT_SPEED = 5f;
+	// Use this for initialization
+	void Start () {
 		JUMP_SPEED = 8f;
 		playerBody = gameObject.GetComponent<Rigidbody2D>();
 		animator = gameObject.GetComponent<Animator>();
@@ -23,40 +20,28 @@ public class PlayerControls : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (!pressedJump)
-        {
-            pressedJump = Input.GetButtonDown("Jump");
-            releasedJump = false;
-        }
-        if (!releasedJump)
-            releasedJump = Input.GetButtonUp("Jump");
+		if (!pressedJump)
+		{
+			pressedJump = Input.GetButtonDown("Jump");
+			releasedJump = false;
+		}
+		if (!releasedJump)
+			releasedJump = Input.GetButtonUp("Jump");
 
 		if (playerBody.velocity.y < 0)
 		{
 			animator.ResetTrigger("Land");
 			animator.SetTrigger("Fall");
 		}
-    }
+	}
 
 	void FixedUpdate()
 	{
-		float playerMovementDir = Input.GetAxisRaw("Horizontal");
-		playerBody.velocity = new Vector2(playerMovementDir * MOVEMENT_SPEED, playerBody.velocity.y);
-		if (playerMovementDir == 0)
-		{
-			animator.SetTrigger("Stop");
-		}
-		else
-		{
-			animator.SetTrigger("Run");
-			gameObject.GetComponent<SpriteRenderer>().flipX = playerMovementDir < 0;
-		}
-
 		int defaultLayer = 1 << LayerMask.NameToLayer("Default");
 		RaycastHit2D hit = Physics2D.Raycast(playerBody.transform.position, Vector2.down, 100, defaultLayer);
 		if (hit.distance > gameObject.GetComponent<Renderer>().bounds.size.y / 2 + 0.2)
 		{
-		//	animator.ResetTrigger("Fall");
+			//	animator.ResetTrigger("Fall");
 			animator.SetTrigger("Jump");
 		}
 
@@ -74,11 +59,11 @@ public class PlayerControls : MonoBehaviour {
 			animator.SetTrigger("Land");
 		}
 
-        if (releasedJump)
-        {
-            releasedJump = false;
-            if (playerBody.velocity.y > 0)
-                playerBody.velocity = new Vector2(playerBody.velocity.x,playerBody.velocity.y/2);
-        }
+		if (releasedJump)
+		{
+			releasedJump = false;
+			if (playerBody.velocity.y > 0)
+				playerBody.velocity = new Vector2(playerBody.velocity.x, playerBody.velocity.y / 2);
+		}
 	}
 }
---
+++ Ludum Dare 35/Assets/PlayerMovementControls.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovementControls : MonoBehaviour {
+
+	[Range(0f, 25f)]
+	public float MOVEMENT_SPEED;
+
+	Rigidbody2D playerBody;
+	Animator animator;
+
+    // Use this for initialization
+    void Start () {
+		MOVEMENT_SPEED = 5f;
+		playerBody = gameObject.GetComponent<Rigidbody2D>();
+		animator = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    }
+
+	void FixedUpdate()
+	{
+		float playerMovementDir = Input.GetAxisRaw("Horizontal");
+		playerBody.velocity = new Vector2(playerMovementDir * MOVEMENT_SPEED, playerBody.velocity.y);
+		if (playerMovementDir == 0)
+		{
+			animator.SetTrigger("Stop");
+		}
+		else
+		{
+			animator.SetTrigger("Run");
+			gameObject.GetComponent<SpriteRenderer>().flipX = playerMovementDir < 0;
+		}
+	}
+}
