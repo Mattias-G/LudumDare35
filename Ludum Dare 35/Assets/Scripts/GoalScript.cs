f880b1b98779dd1f3c2574097e58d87468381913
Sebastian-0 <melon.bag@hotmail.com>
2016-04-16 19:29:21 +0200

    Added Goal script

---
+++ Ludum Dare 35/Assets/Scripts/GoalScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoalScript : MonoBehaviour {
+
+	public string NextLevel;
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		enterGoal(collider.gameObject);
+	}
+
+	void OnCollision2D(Collision2D collision)
+	{
+		enterGoal(collision.gameObject);
+	}
+
+	void enterGoal(GameObject other)
+	{
+		if (gameObject.tag == "Player")
+		{
+			UnityEngine.SceneManagement.SceneManager.LoadScene(NextLevel);
+		}
+	}
+}
--- Ludum Dare 35/Assets/Scripts/HazardScript.cs
+++ Ludum Dare 35/Assets/Scripts/HazardScript.cs
@@ -3,20 +3,11 @@ using System.Collections;
 
 public class HazardScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
 	void OnCollisionEnter2D(Collision2D other)
 	{
+		string currentScene = UnityEngine.SceneManagement.SceneManager.GetActiveScene().name;
 		if (other.gameObject.tag == "Player")
-			UnityEngine.SceneManagement.SceneManager.LoadScene("PlayerTest");
+			UnityEngine.SceneManagement.SceneManager.LoadScene(currentScene);
 	}
 
 }
