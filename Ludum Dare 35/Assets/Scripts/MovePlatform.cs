a76fdfe04acdc55a781623810f7cb1fcd939706f
Mattias Gustafsson <dat13mgu@student.lu.se>
2016-04-17 12:46:01 +0200

    Added moving platforms

---
+++ Ludum Dare 35/Assets/Scripts/MovePlatform.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePlatform : MonoBehaviour {
+
+	public float initialHorizontalVelocity;
+	public float initialVerticalVelocity;
+
+	private Rigidbody2D body;
+
+	void Start () {
+		body = gameObject.GetComponent<Rigidbody2D>();
+		body.velocity = new Vector2(initialHorizontalVelocity, initialVerticalVelocity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Ludum Dare 35/Assets/Scripts/TurnMovingPlatform.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurnMovingPlatform : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Moving Platform")
+		other.gameObject.GetComponent<Rigidbody2D>().velocity = -other.gameObject.GetComponent<Rigidbody2D>().velocity;
+	}
+}
