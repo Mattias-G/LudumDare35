3d694efe590f7c264bb6540a89825e15a5e8d807
Markus-O <markus.iluvatar@gmail.com>
2016-04-17 14:12:12 +0200

    Added Trampolines

---
+++ Ludum Dare 35/Assets/Scripts/TrampolineScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrampolineScript : MonoBehaviour
+{
+
+    public float JumpStrength = 10;
+
+    Animator animator;
+
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        var angle = -gameObject.transform.rotation.z;
+        Debug.Log(angle);
+        var velocity = collision.relativeVelocity;
+        velocity = Vector2.Reflect(velocity, new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)));
+        if (velocity.magnitude < JumpStrength)
+        {
+            velocity.Normalize();
+            velocity *= JumpStrength;
+        }
+        collision.rigidbody.velocity = velocity;
+
+        if (animator != null)
+        {
+            animator.SetTrigger("Hit");
+        }
+    }
+}
