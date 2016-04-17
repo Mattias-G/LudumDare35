d07fe715433b7b8f1c61e0fc8ede5251e9d6a67d
Sebastian Hjelm <melon.bag@hotmail.com>
2016-04-17 18:44:03 +0200

    Added a pause menu

---
+++ Ludum Dare 35/Assets/Scripts/Pause menu/ButtonListener.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Pausemenu {
+    public class ButtonListener : MonoBehaviour
+    {
+        void Start()
+        {
+            Time.timeScale = 0;
+        }
+
+        public void ResumeGame()
+        {
+            Time.timeScale = 1;
+            Destroy(gameObject);
+        }
+
+        public void OpenMainMenu()
+        {
+            Time.timeScale = 1;
+            UnityEngine.SceneManagement.SceneManager.LoadScene("main_menu");
+        }
+
+        public void ExitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
