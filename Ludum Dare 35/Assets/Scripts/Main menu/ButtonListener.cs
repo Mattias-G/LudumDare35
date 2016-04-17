60fb586ebd1db9475220ee46df11962382d0bd8b
Sebastian Hjelm <melon.bag@hotmail.com>
2016-04-17 13:52:42 +0200

    Added a simple main menu

---
+++ Ludum Dare 35/Assets/Scripts/Main menu/ButtonListener.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonListener : MonoBehaviour {
+    
+	public void startGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("PlayerTest");
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
