9e13d641cd095ea83d65e672c066a41857f8c3b1
Sebastian Hjelm <melon.bag@hotmail.com>
2016-04-18 22:31:15 +0200

    Added a button to the thanks for playing screen

---
+++ Ludum Dare 35/Assets/Scripts/Thanks menu/ButtonListener.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Thanksmenu
+{
+	public class ButtonListener : MonoBehaviour
+	{
+		public void OpenMainMenu()
+		{
+			UnityEngine.SceneManagement.SceneManager.LoadScene("main_menu");
+		}
+	}
+}
